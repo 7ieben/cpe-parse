1e4ef7ec62cb9f2259932d866c69b76541a999ec
Steven Kutsch <kutsch.steven@gmail.com>
2014-02-09 15:32:19 +0100

    added Read instance for CPERecord; removed parseCPE

--- src/CPEParse.hs
+++ src/CPEParse.hs
@@ -1,7 +1,4 @@
-module CPEParse 
-    ( CPERecord
-    , parseCPE
-    ) where
+module CPEParse (CPERecord) where
 
 import Text.Printf (printf)
 
@@ -20,21 +17,17 @@ instance Show CPERecord where
                  (part cpe) (vendor cpe) (CPEParse.product cpe) (version cpe)
                  (update cpe) (edition cpe) (language cpe)
 
-split :: Eq a => a -> [a] -> [[a]]
-split _ [] = []
-split delim l = let (init,remainder) = span (/= delim) l
-                in init : case remainder of
-                            [] -> []
-                            x -> split delim (tail remainder)
-
-parseCPE :: String -> CPERecord
-parseCPE s = CPERecord part vendor product version update edition language
-    where
-      cpeList = split ':' $ drop 5 s
-      part = cpeList !! 0
-      vendor = cpeList !! 1
-      product = cpeList !! 2
-      version = cpeList !! 3
-      update = read $ cpeList !! 4
-      edition = cpeList !! 5
-      language = cpeList !! 6
+instance Read CPERecord where
+    readsPrec _ cpeString =
+        case split ':' cpeString of
+          [_, part, vendor, product, version, update, edition, language]
+              -> [(CPERecord (tail part) vendor product 
+                             version (read update) edition language,[])]
+          x -> error $ "Wrong format in CPE String: " ++ show x
+        where
+          split :: Eq a => a -> [a] -> [[a]]
+          split _ [] = []
+          split delim l = let (init,remainder) = span (/= delim) l
+                          in init : case remainder of
+                                      [] -> []
+                                      x -> split delim (tail remainder)
